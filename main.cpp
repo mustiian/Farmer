535755a69288527ad5fe32cc35faa36b5ec5e28b
mustiian <ianmustiats@seznam.cz>
2018-10-15 12:04:11 +0200

    Add new classes Fairs and FarmFairs

--- main.cpp
+++ main.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+#include <map>
+#include <set>
+
+using namespace std;
+
+
+class CFarmFair{
+public:
+    CFarmFair() {}
+    ~CFarmFair() {}
+
+    int getMeal() { return m_Meal; }
+    void setMeal ( const int& meal ) { m_Meal = meal; }
+
+    void AddNeighbor ( const int& neighbor ){
+        m_Neighbors.insert( m_Neighbors.begin(), neighbor );
+    }
+
+    set<int> getNeighborsOfFair ( ) {
+        return m_Neighbors;
+    }
+
+private:
+    int m_Meal;
+    set<int> m_Neighbors;
+};
+
+class CFairs{
+public:
+    CFairs() {}
+    ~CFairs() {}
+
+    set<int> getNeighborsOfFair ( const int& number ){
+        return m_Fairs[ number ].getNeighborsOfFair();
+    }
+
+
+private:
+    int m_SumCities;
+    int m_SumConnetions;
+    int m_SumMeals;
+    int m_MinimumMeals;
+
+    map< int, CFarmFair > m_Fairs;
+};
+
 
 
 int main(){
-   std::cout << "Hello world" << std::endl;
+
 }
